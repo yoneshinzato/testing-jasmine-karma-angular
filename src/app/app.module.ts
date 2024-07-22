4a564f1373a087e47eb48150d034ee4a32a8f391
Yone <chibariyo@gmail.com>
2024-07-22 12:30:11 -0300

    unit testing with jasmine and karma

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StubComponent } from './stub/stub.component';
@@ -12,6 +12,7 @@ import { StubComponent } from './stub/stub.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
